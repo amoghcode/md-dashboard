6a2fd00347bb334008cf9598c57ff09d0cb6564d
Amogh <263152362+amoghcode@users.noreply.github.com>
2026-08-08 19:22:34 +0530

    Add SimulationState class

---
+++ models.py
@@ -0,0 +1,17 @@
+from dataclasses import dataclass
+
+@dataclass
+class SimulationState:
+    current_step: int
+    simulated_time_ps: float
+    total_steps: int
+
+    @property
+    def simulated_time_ns(self) -> float:
+        return self.simulated_time_ps / 1000
+
+    @property
+    def progress_percent(self) -> float:
+        if self.total_Steps <= 0:
+            return 0.0
+        return 100 * self.current_step / self.total_steps
